d22d2c9cf0cc6b1c1873a067f96f025c2b291edc
Baptiste Jamin <baptistejamin@gmail.com>
2015-06-03 17:17:02 +0200

    Fixed toolbar style

--- app/src/main/java/net/randomail/activities/EmailsActivity.java
+++ app/src/main/java/net/randomail/activities/EmailsActivity.java
@@ -23,6 +23,7 @@ import net.randomail.R;
 import net.randomail.RandomailApplication;
 import net.randomail.adapters.EmailsAdapter;
 import net.randomail.helpers.Clipboard;
+import net.randomail.helpers.PreferencesHelper;
 import net.randomail.models.Email;
 import net.randomail.models.User;
 import net.randomail.services.ApiServices;
@@ -112,13 +113,15 @@ public class EmailsActivity extends AppCompatActivity implements MaterialTabList
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.logout) {
+            PreferencesHelper.clear();
+            Intent intent = new Intent(this, LoginActivity.class);
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
             return true;
         }
 
